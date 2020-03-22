028f459d0d617b6fce76d9bd4ef2b784be466db0
hill <ql1119079560@yahoo.com>
2020-03-22 18:44:17 +0800

    update alt speed up

--- main.py
+++ main.py
@@ -2,20 +2,22 @@
 
 import pynput
 class YGlobal(object):
+    speed_up = 6
     dd = 9
-    left_key_ = '\''
-    right_key_ = ';'
-    move_left_key_ = 'm'
-    move_down_key_ = ','
-    move_up_key_ = '.'
-    move_right_key_ = '/'
+    left_key_ = 222 #'\''
+    right_key_ = 186 #';'
+    move_left_key_ = 77 #'\r'
+    move_down_key_ = 188 #','
+    move_up_key_ = 190 #'.'
+    move_right_key_ = 191 #'/'
     ctrl_flag_ = False
     mouse_controller_ = pynput.mouse.Controller()
     left_flag_ = False
     right_flag_ = False
+    alt_flag_ = False
 
 def IsKey(key, ch):
-    if key == pynput.keyboard.KeyCode.from_char(ch):
+    if hasattr(key, "vk") and key.vk == ch:
         return True
     else:
         return False
@@ -37,8 +39,12 @@ def ButtonRight(press_flag):
     return None
 
 def OnPress(key):
-    print('{0} press'.format(key)) #
-    if (key == pynput.keyboard.Key.ctrl_l or key == pynput.keyboard.Key.ctrl_r) and not YGlobal.ctrl_flag_:
+    # print('{0} press'.format(key))
+    if (key == pynput.keyboard.Key.alt_l or key == pynput.keyboard.Key.alt_r) \
+        and not YGlobal.alt_flag_:
+        YGlobal.alt_flag_ = True
+    if (key == pynput.keyboard.Key.ctrl_l or key == pynput.keyboard.Key.ctrl_r) \
+        and not YGlobal.ctrl_flag_:
         YGlobal.ctrl_flag_ = True
     elif YGlobal.ctrl_flag_:
         if not YGlobal.left_flag_ and IsKey(key, YGlobal.left_key_):
@@ -46,18 +52,32 @@ def OnPress(key):
         elif not YGlobal.right_flag_ and IsKey(key, YGlobal.right_key_):
             ButtonRight(True)
         elif IsKey(key, YGlobal.move_down_key_):
-            YGlobal.mouse_controller_.move(0,YGlobal.dd)
+            if YGlobal.alt_flag_:
+                YGlobal.mouse_controller_.move(0,YGlobal.dd*YGlobal.speed_up)
+            else:
+                YGlobal.mouse_controller_.move(0,YGlobal.dd)
         elif IsKey(key, YGlobal.move_up_key_):
-            YGlobal.mouse_controller_.move(0,-YGlobal.dd)
+            if YGlobal.alt_flag_:
+                YGlobal.mouse_controller_.move(0,-YGlobal.dd*YGlobal.speed_up)
+            else:
+                YGlobal.mouse_controller_.move(0,-YGlobal.dd)
         elif IsKey(key, YGlobal.move_left_key_):
-            YGlobal.mouse_controller_.move(-YGlobal.dd,0)
+            if YGlobal.alt_flag_:
+                YGlobal.mouse_controller_.move(-YGlobal.dd*YGlobal.speed_up,0)
+            else:
+                YGlobal.mouse_controller_.move(-YGlobal.dd,0)
         elif IsKey(key, YGlobal.move_right_key_):
-            YGlobal.mouse_controller_.move(YGlobal.dd,0)
+            if YGlobal.alt_flag_:
+                YGlobal.mouse_controller_.move(YGlobal.dd*YGlobal.speed_up,0)
+            else:
+                YGlobal.mouse_controller_.move(YGlobal.dd,0)
 
 def OnRelease(key):
-    print('{0} release'.format(key))
+    # print('{0} release'.format(key))
     if key == pynput.keyboard.Key.scroll_lock:
         return False
+    elif key == pynput.keyboard.Key.alt_l or key == pynput.keyboard.Key.alt_r:
+        YGlobal.alt_flag_ = False
     elif key == pynput.keyboard.Key.ctrl_l or key == pynput.keyboard.Key.ctrl_r:
         YGlobal.ctrl_flag_ = False
         if YGlobal.left_flag_:
@@ -78,7 +98,8 @@ Ctrl + '.    鼠标向上移动
 Ctrl + '/'    鼠标向右移动
 Ctrl + ':'    按下鼠标右键
 Ctrl + '''    按下鼠标左键
-Scroll Lock    终止程序""")
+Scroll Lock    终止程序
+(Alt键可以6倍速移动)""")
 
 with pynput.keyboard.Listener(on_press = OnPress,on_release = OnRelease) as listener:
     listener.join()
